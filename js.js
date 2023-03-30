572231cb0f225a7cff204098c4ba0ca69955ff84
SitingXing <121953533+SitingXing@users.noreply.github.com>
2023-03-30 17:16:13 -0400

    Add files via upload

---
+++ js.js
@@ -0,0 +1,9 @@
+const theButton = document.querySelector("button");
+const output = document.querySelector('div');
+
+const buttonCss = document.styleSheets[0].cssRules[0].cssRules[1];
+
+console.log(buttonCss.parentRule)
+
+console.log(buttonCss.parentStyleSheet)
+
