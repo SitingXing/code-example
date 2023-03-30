225a7ad5d6a1586c10b7cf8fd830a294d02e94db
SitingXing <121953533+SitingXing@users.noreply.github.com>
2023-03-30 17:09:10 -0400

    Update js.js

--- js/js.js
+++ js/js.js
@@ -4,3 +4,5 @@ const output = document.querySelector('div');
 const buttonCss = document.styleSheets[0].cssRules[0].cssRules[1];
 
 console.log(buttonCss.parentRule)
+
+console.log(buttonCss.parentStyleSheet)
